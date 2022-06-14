b4a206cc881f1c660c3b4acb97b398b47f27cbb6
Emma Karklins <ekarklins@ssmic.com>
2022-06-14 09:28:41 -0400

    Add project files.

---
+++ CatchTheAce/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace CatchTheAce
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> deck = new List<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                int random = new Random().Next(0, 52);
+                while (!deck.Contains(random))
+                {
+                    deck.Add(random);
+                }
+            }
+
+            foreach (int d in deck)
+            {
+                Console.WriteLine(d);
+            }
+        }
+    }
+
+}
+
