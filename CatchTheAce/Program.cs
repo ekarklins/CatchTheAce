f2eb691158f992970cc759790a8d2d9996b22ffa
Emma Karklins <ekarklins@ssmic.com>
2022-06-15 16:40:38 -0400

    Created CreateCollection method. Extended DeckCollection class from DeckBuilder class

--- CatchTheAce/Program.cs
+++ CatchTheAce/Program.cs
@@ -9,7 +9,8 @@ namespace CatchTheAce
         // a method that fills an empty list
     }
 
-    public class DeckBuilder {
+    public class DeckBuilder 
+    {
         public static List<int> deck = new List<int>();
         public List<int> CreateDeck()
         {
@@ -30,12 +31,24 @@ namespace CatchTheAce
     }
    
 
-    public class DeckCollection { }
+    public class DeckCollection : DeckBuilder
+    {
+        public static List<List<int>> collection = new List<List<int>>();
+        public List<List<int>> CreateCollection(int years)
+        {
+            for(int i = 0; i < years; i++)
+            {
+                collection.Add(CreateDeck());
+            }
+            return collection;
+        }
+    }
     // a method that adds a deck to the deck collection (2d list/ 2d array)?
     // if array, set size as number of years?
     //make a recursive call until collection reaches max capacity?
 
-    public class Results {
+    public class Results 
+    {
         public static int count;
         public static List<int> weeks;
         public List<int> GoThroughResults(List<List<int>> collection)
@@ -69,6 +82,8 @@ namespace CatchTheAce
             return ((float)count / (float)years) * 100;
         }
 
+
+
     }
     // a method that goes through the deck collection and logs the results in a list/array?
     // a method that calculates percentage
@@ -83,10 +98,32 @@ namespace CatchTheAce
     {
         public static void Main(string[] args)
         {
-            int years1 = 10;
-            int count1 = 3;
+            List<List<int>> decks = new List<List<int>>(3);
+            List<int> deck1 = new List<int>() { 1, 2, 3, 4, 5 };
+            List<int> deck2 = new List<int>() { 6, 7, 8, 9, 10 };
+            List<int> deck3 = new List<int>() { 11, 12, 13, 14, 15 };
+            List<int> deck4 = new List<int>() { 16, 17, 18, 19, 20 };
 
-            Console.WriteLine(((float)count1 / (float)years1)*100);
+            decks.Add(deck1);
+            decks.Add(deck2);
+            decks.Add(deck3);
+            for (int i = 0; i < decks.Count; i++)
+            {
+                for(int j = 0; j < decks[i].Count; j++)
+                {
+                    Console.WriteLine(decks[i][j]);
+                }
+            }
+            decks.Add(deck4);
+            for (int i = 0; i < decks.Count; i++)
+            {
+                for (int j = 0; j < decks[i].Count; j++)
+                {
+                    Console.WriteLine(decks[i][j]);
+                }
+            }
+
+
 
 
         }
