f68125c267ee20f5c2dee5c963d9808f3975ea6d
Emma Karklins <ekarklins@ssmic.com>
2022-07-27 13:39:40 -0400

    GoThroughResults test is complete with output info displayed in the console.

--- CatchTheAceTests/ResultsTests.cs
+++ CatchTheAceTests/ResultsTests.cs
@@ -12,16 +12,23 @@ namespace CatchTheAceTests
             _testOutputHelper = testOutputHelper;
         }
 
-        [Fact]
-        public void GoThroughResults_ValidInput_ValidResults()
+        [Theory]
+        [InlineData(5)]
+        [InlineData(10)]
+        [InlineData(50)]
+        public void GoThroughResults_ValidInput_ValidResults(int numOfYears)
         {
             var deckBuilder = new DeckBuilder();
-            var validDeck = deckBuilder.InitializeDeck(5);
+            var validDeck = deckBuilder.InitializeDeck(numOfYears);
             deckBuilder.ShufflesAllDecks(validDeck);
             var resultsbuilder = new Results();
             var (count, listOfWeeks) = resultsbuilder.GoThroughResults(validDeck);
+
+            _testOutputHelper.WriteLine("Number of times the Ace was found in the last week: " + count.ToString());
+            _testOutputHelper.WriteLine("List of weeks where ace was found: " + string.Join(",", listOfWeeks));
             Assert.IsType<int>(count);
             Assert.IsType<List<int>>(listOfWeeks);
+
         }
 
 
